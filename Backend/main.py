468ec37fdd3fb4a05c3e913e8493f0a2d4deafd1
CZandt <95643051+CZandt@users.noreply.github.com>
2024-09-17 11:38:42 -0600

    File Structure

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from .endpoints import drive
 
 app = FastAPI()
 
+# Include routers from different endpoint files
+app.include_router(drive.router)
+
+# Root endpoint (optional)
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI 2"}
+    return {"message": "Welcome to FastAPI!"}
