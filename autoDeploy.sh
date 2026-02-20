019357ed9c4c5f2bf328ae6eb588b1fca9c61b94
Ruby-Hunter <rex9832@gmail.com>
2026-02-20 10:16:50 -0700

    autodeploy comments

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+echo "Pulling latest code from git repository..."
 git pull
 
+echo "Running build"
 cd Frontend/ava-02
 npm ci
 NODE_OPTIONS=--max_old_space_size=2048 npm run build
 cd ../..
 
+echo "Restarting docker container"
 docker compose up -d --build
