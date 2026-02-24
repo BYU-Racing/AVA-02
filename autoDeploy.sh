2d24f207dfa258e179136893ff5e532961bd9c61
Ruby-Hunter <rex9832@gmail.com>
2026-02-24 11:25:10 -0700

    Small adjustments for new build

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 echo "Pulling latest code from git repository..."
 git pull
 
-echo "Running build"
-cd Frontend/ava-02
+echo "Running Frontend build via Vite"
+cd Frontend/ava-03
 npm ci
 NODE_OPTIONS=--max_old_space_size=2048 npm run build
 cd ../..
 
 echo "Restarting docker container"
 docker compose up -d --build
+
+echo "Deployment complete!"
