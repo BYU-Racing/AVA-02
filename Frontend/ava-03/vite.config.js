5e63ad5dee99b5c090222f0a21df48585f48279b
Ruby-Hunter <rex9832@gmail.com>
2026-02-24 11:35:51 -0700

    Quick change to config

--- Frontend/ava-03/vite.config.js
+++ Frontend/ava-03/vite.config.js
@@ -14,9 +14,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 
+const ANALYZE = process.env.ANALYZE === "true";
+
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true, filename: "dist/stats.html", gzipSize: true, brotliSize: true }),
+    ...(ANALYZE 
+      ? [visualizer({ open: true, filename: "dist/stats.html", gzipSize: true, brotliSize: true })] 
+      : []),
   ],
 });
