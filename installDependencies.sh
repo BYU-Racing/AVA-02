44b5c7b58eb44745e75c08672d6c0fd269fc3780
Ruby-Hunter <rex9832@gmail.com>
2026-05-16 22:52:27 +0200

    small dependencies fix

--- installDependencies.sh
+++ installDependencies.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 echo "===== Installing Dependencies for AVA-03 ====="
 
 sudo yum update -y
-sudo yum install -y docker git curl
+sudo yum install -y docker git
 sudo systemctl enable --now docker
 sudo usermod -a -G docker ec2-user
 
