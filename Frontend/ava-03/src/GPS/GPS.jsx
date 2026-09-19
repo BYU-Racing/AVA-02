05505aa2b113cd99d2e3bd528a44561d4bfb1974
Jake Schulz <jakeyboom@gmail.com>
2026-09-18 23:10:32 -0600

    Made it so that the map begins zoomed in at the HBLL

--- Frontend/ava-03/src/GPS/GPS.jsx
+++ Frontend/ava-03/src/GPS/GPS.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 //Note: An implementation of the GPS component will be changed over to Leaflet in the future.  Current implementation is just a placeholder
 function GPS() {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{height:"100vh", width:"100vw"}}>
+        <MapContainer center={[40.2488, -111.6495]} zoom={18} scrollWheelZoom={true} style={{height:"100vh", width:"100vw"}}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {/* <Marker position={[51.505, -0.09]}>
+            <Marker position={[40.2488, -111.6495]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
-            </Marker> */}
+            </Marker>
         </MapContainer>
 
     );
